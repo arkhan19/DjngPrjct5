eca2380414ffd812234b6512b80e444e65ab90ab
f3n1xx <arkspeed@gmail.com>
2017-05-05 23:24:50 +0530

    Jquery Message integration

---
+++ static/js/custom.js
@@ -0,0 +1,24 @@
+/**
+ * Created by f3n1xx on 20/04/17.
+ */
+function showFlashMessage(message)
+{
+    var template= "<div class ='container-alert-flash'>"+
+    "<div>"+
+        "<div class='alert alert-success alert-dismissible' role='alert'>"+
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>"+
+                "<span aria-hidden='true'>&times;</span>"+
+            "</button>"+
+            "<strong>"
+            +message+
+            "</strong>..."+
+        "</div>"+
+    "</div>"+
+"</div>"
+
+    $("body").append(template)
+    $('.container-alert-flash').fadeIn();
+    setTimedOut(function(){
+        $('.container-alert-flash').fadeOut();
+    }, 1800);
+}
